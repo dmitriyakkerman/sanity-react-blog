3ec1c186ca0936f16f6b8935b53d2861c98d03a8
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-12-17 17:05:46 +0200

    fresh build

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ export default function HomePage() {
         <div className="rcb-home">
             <div className="container">
                 <h1 className="rcb-home__title">Hey, my name is {document.title}!</h1>
-                <p className="rcb-home__text">Welcome to my blog!</p>
+                <p className="rcb-home__text">Welcome to my blog! <br/>You can find Git repository <a href="https://github.com/dmitriyakkerman/sanity-react-blog/">here</a></p>
             </div>
         </div>
     )
